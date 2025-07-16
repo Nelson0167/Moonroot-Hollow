e157d6a710947f93887ccd81a9c8cc0103d9da6d
Nelson0167 <nelsonj0167@gmail.com>
2025-07-16 13:43:54 -0700

    Branch merge

--- core/src/main/kotlin/com/weirdbees/moonroothollow/LoadingScreen.kt
+++ core/src/main/kotlin/com/weirdbees/moonroothollow/LoadingScreen.kt
@@ -12,7 +12,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable
 import com.badlogic.gdx.utils.viewport.FitViewport
 import com.badlogic.gdx.utils.viewport.Viewport
-import com.sun.tools.javac.Main
 
 class LoadingScreen(private val game: MoonrootHollowGame
 ) : ScreenAdapter() {
